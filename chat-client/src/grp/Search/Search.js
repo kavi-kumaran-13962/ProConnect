46d9374d0c8e5b542a0d9f824f6c20eb9fb6f8cb
Manoj Bhavvan <79741733+manojbhavvan@users.noreply.github.com>
2023-06-29 22:38:24 +0530

    Add files via upload

---
+++ chat-client/src/grp/Search/Search.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import "./Search.css"
+
+const Search = ({ onSearch }) => {
+    const [searchTxt, setsearchTxt] = useState('');
+
+    const handleInputChange = (e) => {
+        setsearchTxt(e.target.value);
+    };
+
+    const handleSend = () => {
+        // Handle sending the searchTxt here
+        onSearch(searchTxt);
+        console.log(searchTxt);
+        setsearchTxt('');
+    };
+
+    return (
+        <div className='search'>
+            <input 
+                type="text" 
+                value={searchTxt} 
+                onChange={handleInputChange} 
+                placeholder="search..."     
+                className='search__inp'
+            />
+            <div 
+                onClick={handleSend} 
+                className='search__sendDiv'
+            >
+                <i className='search__sendicon fa fa-search icon'></i>
+            </div>
+        </div>
+    );
+};
+
+export default Search;
